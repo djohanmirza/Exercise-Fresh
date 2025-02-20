8e25f915baecbe3df78eaa7ce46798cd4d1255e7
Mirza Djohan <mirzadjohan@gmail.com>
2025-02-20 20:34:21 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/djohanmirza/Exercise-Fresh
+
+go 1.24.0
---
+++ logic1/soal1.go
@@ -0,0 +1,9 @@
+package logic1
+
+import "fmt"
+
+func Nomor1() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(2*i+1, " ")
+	}
+}
